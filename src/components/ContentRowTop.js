bda5a873c693d716019df4472f3c673a0d1c47d2
Myllena Lucena <karensantana278@gmail.com>
2022-07-07 22:00:51 -0300

    component painel and cardGenres

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import imagenFondo from '../assets/images/mandalorian.jpg';
 import ContentRowMovies from './ContentRowMovies';
+import GenreCard from './GenreCard';
+import Painel from './Painel';
 
 function ContentRowTop(){
 	const informacoes = [
@@ -9,6 +11,18 @@ function ContentRowTop(){
 		{titulo: "Actors Quantity", quantidade:49, icone:'user', cor:'warning'},
 
 	];
+	const genres = [
+		'Acción', 
+		'Animación',
+		'Aventura',
+		'Ciencia Ficción',
+		'Comedia',
+		'Documental',
+		'Drama',
+		'Fantasia',
+		'Infantiles',
+		'Musical'
+	]
 
     return(
         <React.Fragment>
@@ -36,105 +50,21 @@ function ContentRowTop(){
 					{/*<!-- Content Row Last Movie in Data Base -->*/}
 					<div className="row">
 						{/*<!-- Last Movie in DB -->*/}
-						<div className="col-lg-6 mb-4">
-							<div className="card shadow mb-4">
-								<div className="card-header py-3">
-									<h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
-								</div>
-								<div className="card-body">
+						<Painel titulo="Last movie in Data Base">
 									<div className="text-center">
 										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
 									</div>
 									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
 									<a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
-								</div>
-							</div>
-						</div>
-						{/*<!-- End content row last movie in Data Base -->*/}
+						</Painel>
 
 						{/*<!-- Genres in DB -->*/}
-						<div className="col-lg-6 mb-4">						
-							<div className="card shadow mb-4">
-								<div className="card-header py-3">
-									<h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
-								</div>
-								<div className="card-body">
-									<div className="row">
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Acción
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Animación
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Aventura
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Ciencia Ficción
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Comedia
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Documental
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Drama
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Fantasia
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Infantiles
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Musical
-												</div>
-											</div>
-										</div>
-
-									</div>
-								</div>
-							</div>
+						<Painel titulo="Genres in Data Base">
+						<div className="row">
+							{genres.map(genre => <GenreCard name={genre}/>)}	
 						</div>
+						</Painel>
+						
 					</div>
 				</div>
 				{/*<!--End Content Row Top-->*/}
---
+++ src/components/GenreCard.js
@@ -0,0 +1,13 @@
+function GenreCard(props){
+    const {name} = props;
+
+    return <div className="col-lg-6 mb-4">
+        <div className = 'card bg-dark text-white shadow'>
+            <div className = 'card-body'>
+            {name}
+            </div>
+        </div>
+    </div>
+}
+
+export default GenreCard;
